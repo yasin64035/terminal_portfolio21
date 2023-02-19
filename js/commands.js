ca23089c13c75d3a22ea45fd85cde14acc7c551c
md ariful islam <105654052+yasin64035@users.noreply.github.com>
2023-02-19 23:10:57 +0600

    updated
    ten

--- js/commands.js
+++ js/commands.js
@@ -33,7 +33,7 @@ about = [
   "&nbsp;",
   "<span style='color:red;'>01101000 01101001</span> There 👋, I'm <span style='color: yellow;'>Ariful Islam</span> ",
   "&nbsp;",
-  " MOST HANDSOME MEN IN BANGLADESH: ARIFUL ISLAM",
+  " MOST HANDSOME Teenager IN BANGLADESH: ARIFUL ISLAM",
   "   Fitness Model",
   "Greetings, I am a web developer, a young and ambitious ",
   "individual with a passion for crafting innovative solutions for ",
